d4481dfd3086e32de3f90dc3d031a49ea73ecc83
Gautham Vanjre <vanjregautham@gmail.com>
2023-01-01 14:40:18 +0530

    completed header in mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../images/logo.svg";
+import hamburger_open from "../../images/icon-hamburger.svg";
+import hamburger_close from "../../images/icon-close.svg";
 import "./Header.css";
 
 const Header = () => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
   return (
     <header>
-      <nav className="navbar">
+      <nav className={hamburgerOpen ? "menu-bar navbar" : "navbar"}>
         <div className="logo-wrapper">
           <img src={logo} alt="logo" />
         </div>
-        <div className="nav-links">
+
+        <div
+          className={hamburgerOpen ? "nav-links nav-links-close" : "nav-links"}
+        >
           <ul>
             <li>About</li>
             <li>Careers</li>
@@ -17,6 +23,28 @@ const Header = () => {
             <li>Products</li>
             <li>Support</li>
           </ul>
+        </div>
+        <div className="hamburger-icon-wrapper">
+          <img
+            className={
+              hamburgerOpen ? "hamburger-open-close" : "hamburger-close-open"
+            }
+            onClick={() => {
+              setHamburgerOpen(!hamburgerOpen);
+            }}
+            src={hamburger_open}
+            alt="hamburger icon"
+          />
+          <img
+            className={
+              hamburgerOpen ? "hamburger-close-open" : "hamburger-open-close"
+            }
+            onClick={() => {
+              setHamburgerOpen(!hamburgerOpen);
+            }}
+            src={hamburger_close}
+            alt="hamburger close icon"
+          />
         </div>
       </nav>
 
